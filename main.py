df32a56e5ff52b18445ad1b9f4a91dd0d9be837d
ryanrych <ryanjrychlak@gmail.com>
2022-01-04 14:28:03 -0600

    Used Selenium Webdriver to scrape the price data from camelcamelcamel

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pandas as pd
-import requests
-from bs4 import BeautifulSoup
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def generateSKUASPPairs():
     skus = pd.read_excel("products_table.xlsx", usecols="A").transpose()
@@ -32,7 +35,25 @@ SKUASINPairs = generateSKUASINPairs()
 
 for pair in SKUASINPairs:
 
+    driver = webdriver.Chrome('C:\\Users\\ryanj\..PROGRAMS\Python\Acurite-Amazon-Scraper\chromedriver')
+
     url = "https://camelcamelcamel.com/product/" + SKUASINPairs[pair]
 
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content)
+    print("ASIN:", SKUASINPairs[pair])
+
+    driver.get(url)
+    # sleep(5)
+    # priceTag = driver.find_element_by_xpath("//span[@class=\"stat\"]")
+    priceTag = ""
+    try:
+        priceTag = WebDriverWait(driver, 100).until(
+            EC.presence_of_element_located((By.XPATH, "//span[@class=\"stat\"]"))
+        )
+    except:
+        pass
+    finally:
+        if priceTag == "":
+            driver.close()
+        else:
+            print(priceTag.text)
+            driver.close()
